3af597fe9c408ec3a7be5b5ff3b1e58b65740b09
agamonch <jnkyrgyz@gmail.com>
2021-03-28 20:01:29 +0300

    added text 'privet vsem' and buttons

--- app/src/main/java/kg/tutorialapp/bishweather/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/bishweather/MainActivity.kt
@@ -2,10 +2,19 @@ package kg.tutorialapp.bishweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mText = findViewById<TextView>(R.id.mText)
+        mText.text = "Privet Vsem"
+        mText.setTextSize(TypedValue.COMPLEX_UNIT_PX, 30F)
+        val but1 = findViewById<Button>(R.id.button1)
+        val but2 = findViewById<Button>(R.id.button2)
+
     }
 }
